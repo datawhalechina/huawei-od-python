62cb0a0a08233607b43b943f2cd71e2e24dc6482
Yalin Feng <1070298513@qq.com>
2023-08-10 00:40:49 +0800

    others100题 144~145 code/doc

--- codes/others100/144_champions-and-runners-up-in-competition.py
+++ codes/others100/144_champions-and-runners-up-in-competition.py
@@ -12,21 +12,24 @@ from typing import List
 
 def solve_method(strengh: List[int]) -> str:
     nums = len(strengh)
-
-    # 初始顺序
-    rank = list(range(0, nums))
-
+    rank = list(range(0, nums))  # 初始顺序, 代表还要比赛的选手
+    win = []  # 每一轮的胜者
+    lose = []  # 和已经产生的失败者
+    semi_final = {}  # 半决赛 对阵情况(便于输出季军)
     # 需要比较的轮次：n= log2(nums)的向上取整=log2(nums-1)向下取整+1
     n = int(math.log2(nums - 1)) + 1
 
-    # 每一轮的胜者和已经产生的失败者
-    win = []
-    lose = []
-
-    # 模拟法；模拟两两比赛的过程
+    # 模拟法；模拟运动员两两比赛的过程，需要比赛n轮
     for epoch in range(0, n):
-        i = 0
+        # 如果还需比赛的选手不足 4人(半决赛)，为了便于输出季军，保存此时他们的对阵情况
+        if 3 <= len(rank) <= 4:
+            semi_final = {rank[0]: rank[1], rank[1]: rank[0]}
+            if len(rank) == 4:
+                semi_final.update({rank[2]: rank[3], rank[3]: rank[2]})
 
+        # while 循环模拟每一轮比赛
+        win.clear()
+        i = 0
         while i < len(rank):
             player_id = rank[i]
 
@@ -46,20 +49,24 @@ def solve_method(strengh: List[int]) -> str:
                     lose.append(player_id)
                 # 同时处理了2个运动员;
                 i += 2
-        rank = win[:]
-        # 失败者不清空;但这一轮的胜利者要清空
-        win.clear()
 
-    # rank[]里只包含冠军的id,lose[]的倒数第一位是亚军
-    ret = [str(rank[0]), str(lose[-1])]
+        # 本轮胜出的选手参加下一轮比赛
+        rank = win[:]
+
+    # 循环终止时,rank[]里只包含冠军的id,lose[]的倒数第一位是亚军
+    # 季军可以用之前的 semi_final 字典找到
+    champion = rank[0]
+    second_player = lose[-1]
+    third_player = semi_final.get(champion, semi_final.get(second_player))
+    ret = [str(champion), str(second_player), str(third_player)]
     return " ".join(ret)
 
 
 if __name__ == '__main__':
     res = solve_method([2, 3, 4, 5])
     print(res)
-    print(res == "3 1")
+    print(res == "3 1 2")
 
-    res2 = solve_method([6, 5, 4, 3, 2, 1])
+    res2 = solve_method([7, 6, 5, 4, 3, 2, 1, 0])
     print(res2)
-    print(res2=="0 4")
+    print(res2 == "0 4 2")
---
+++ codes/others100/145_competition_rating.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Yalin Feng
+@file: 145_competition_rating.py
+@time: 2023/8/9 23:45
+@project: huawei-od-python
+@desc: 145 比赛评分
+"""
+
+from typing import List
+from functools import cmp_to_key
+
+
+def solve_method(M: int, N: int, ratings: List[List[int]]) -> str:
+    # 1. 检查输入参数是否满足要求(M,N,评分)
+    if not (3 <= M <= 10) or not (3 <= N <= 100):
+        return "-1"
+    for row in ratings:
+        if not all(1 <= num <= 10 for num in row):
+            return "-1"
+
+    # 2.用 *得分频数矩阵* scores 记录选手得分情况
+    # scores[i][j] 表示运动员 i 得到分数 j 的次数, scores[i][0] 表示运动员 i 得到的总分
+    scores = [[0] * 11 for _ in range(101)]
+
+    # 3.遍历评分,记录分数
+    for i in range(M):
+        for j in range(N):
+            score = ratings[i][j]
+
+            # 运动员编号从 1 到 N
+            player_id = j + 1
+            scores[player_id][0] += score  # 运动员总分增加
+            scores[player_id][score] += 1  # 运动员的某个分数频数+1
+
+    # 4. 自定义排序规则
+    def compare_function(id1: int, id2: int) -> int:
+        player1 = scores[id1]  # 通过运动员编号id,让player保存每个运动员的得分情况
+        player2 = scores[id2]
+        if player1[0] != player2[0]:  # player1[0]是1号运动员的总分
+            return -1 * (player1[0] - player2[0])  # -1 是逆序; 让总分高的排前边
+
+        # 接下来比较得分高分值的频数
+        for score in range(10, -1, -1):
+            if player1[score] != player2[score]:
+                return -1 * (player1[score] - player2[score])  # -1 是逆序; 让得分高分值最多的选手排名靠前
+
+        return 0
+
+    # 5.根据记录的分数 scores 和我们定义的排序规则排序(第一名，第二名，...)
+    rank = sorted(range(1, N + 1), key=cmp_to_key(compare_function))
+
+    # 6.将前三名的id转化为string后返回
+    return ",".join([str(rank[0]), str(rank[1]), str(rank[2])])
+
+
+if __name__ == '__main__':
+    ratings1 = [
+        [10, 6, 9, 7, 6],
+        [9, 10, 6, 7, 5],
+        [8, 10, 6, 5, 10],
+        [9, 10, 9, 4, 9],
+    ]
+    res1 = solve_method(4, 5, ratings1)
+    print(res1)
+    print(res1 == "2,1,5")
+
+    ratings2 = [
+        [7, 3, 5, 4, 2],
+        [8, 5, 4, 4, 3],
+    ]
+    res2 = solve_method(2, 5, ratings2)
+    print(res2)
+    print(res2 == "-1")
+
+    ratings3 = [
+        [8, 5],
+        [5, 6],
+        [10, 4],
+        [8, 9],
+    ]
+    res3 = solve_method(4, 2, ratings3)
+    print(res3)
+    print(res3 == "-1")
+
+    ratings4 = [
+        [11, 6, 9, 7, 8],
+        [9, 10, 6, 7, 8],
+        [8, 10, 6, 9, 7],
+        [9, 10, 8, 6, 7],
+    ]
+    res4 = solve_method(4, 5, ratings4)
+    print(res4)
+    print(res4 == "-1")
