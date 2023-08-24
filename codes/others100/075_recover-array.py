cd1f0002bafae9971d23d780f1063a616f617c61
zhangchao <amzcso@163.com>
2023-08-24 18:55:29 +0800

    完成others100-075-恢复数字序列

--- codes/others100/075_recover-array.py
+++ codes/others100/075_recover-array.py
@@ -9,18 +9,41 @@
 from collections import Counter
 
 
-# def solve_method(s, n):
-#     length = len(s)
-#     counter = sorted(dict(Counter(s)).items(), key=lambda x: x[1], reverse=True)
-#     if length % n == 0:
-#         k = length // n
-#         if k == 1:
-#             return int(sorted(list(s))[0])
-#         elif k == 2:
-#
-#
-#         pass
-#     else:
-#         k = length // n
-#         s0 = '9' * k
-#         diff = (len(s0) + 1) * n
+def is_same_dict(d1, d2):
+    for key in d1:
+        if key not in d2 or d1[key] != d2[key]:
+            return False
+    for key in d2:
+        if key not in d1 or d1[key] != d2[key]:
+            return False
+    return True
+
+
+def solve_method(s, n):
+    counter = dict(Counter(s))
+    candidate = [val for val in range(n)]
+    candidate_counter = dict(Counter(''.join(map(str, candidate))))
+    for new in range(n, 1001):
+        if is_same_dict(candidate_counter, counter):
+            return candidate[0]
+        else:
+            prev = candidate.pop(0)
+            for key in str(prev):
+                candidate_counter[key] -= 1
+                if candidate_counter[key] == 0:
+                    candidate_counter.pop(key)
+
+            candidate.append(new)
+            for key in str(new):
+                if key not in candidate_counter:
+                    candidate_counter[key] = 1
+                else:
+                    candidate_counter[key] += 1
+    return -1
+
+
+if __name__ == '__main__':
+    s, n = input().strip().split(' ')
+    n = int(n)
+    res = solve_method(s, n)
+    print(res)
--- docs/others100/075_recover-array.md
+++ docs/others100/075_recover-array.md
@@ -34,11 +34,60 @@
 还原出的序列为8 9 10 11 12，故输出8
 
 ## 解题思路
-
+滑动窗口，遍历所有n个连续数字组成的数组，统计字符出现次数是否与目标次数一致
 
 ## 解题代码
 
 ```python
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author:  zhangchao
+@file: 075_recover-array
+@time:  24/8/2023 下午 5:04
+@project:  huawei-od-python 
+"""
+from collections import Counter
+
+
+def is_same_dict(d1, d2):
+    for key in d1:
+        if key not in d2 or d1[key] != d2[key]:
+            return False
+    for key in d2:
+        if key not in d1 or d1[key] != d2[key]:
+            return False
+    return True
+
+
+def solve_method(s, n):
+    counter = dict(Counter(s))
+    candidate = [val for val in range(n)]
+    candidate_counter = dict(Counter(''.join(map(str, candidate))))
+    for new in range(n, 1001):
+        if is_same_dict(candidate_counter, counter):
+            return candidate[0]
+        else:
+            prev = candidate.pop(0)
+            for key in str(prev):
+                candidate_counter[key] -= 1
+                if candidate_counter[key] == 0:
+                    candidate_counter.pop(key)
+                    
+            candidate.append(new)
+            for key in str(new):
+                if key not in candidate_counter:
+                    candidate_counter[key] = 1
+                else:
+                    candidate_counter[key] += 1
+    return -1
+
+
+if __name__ == '__main__':
+    s, n = input().strip().split(' ')
+    n = int(n)
+    res = solve_method(s, n)
+    print(res)
 
 
 ```
